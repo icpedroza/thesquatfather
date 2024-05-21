a045b33b0e35c65f62b2e3d8fa07cdb80439464a
Ian Pedroza <ipedroza@wharton.upenn.edu>
2024-05-21 14:48:37 +0800

    base app.py and app.js

--- chakra-ui-app/src/App.js
+++ chakra-ui-app/src/App.js
@@ -1,38 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import { Box, Flex, Button, VStack, Text } from '@chakra-ui/react';
+import VideoFeed from './VideoFeed';
 
 function App() {
-
-  const [data, setData] = useState({});
-  
-  useEffect(() => {
-    fetch('http://localhost:5000/api/data')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div style={{ display: 'flex', flexWrap: 'wrap', height: '100vh' }}>
+                <div style={{ flex: '1 0 50%', border: '1px solid black' }}>
+                    <VideoFeed />
+                </div>
+                <div style={{ flex: '1 0 50%', border: '1px solid black' }}>
+                    {/* Another component or content */}
+                </div>
+                <div style={{ flex: '1 0 50%', border: '1px solid black' }}>
+                    {/* Another component or content */}
+                </div>
+                <div style={{ flex: '1 0 50%', border: '1px solid black' }}>
+                    {/* Another component or content */}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
